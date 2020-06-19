36977c43b94bfaed5a28ff5a0bffe31e84b17b0b
Mayank Gautam <mgautam099@gmail.com>
2020-06-20 01:43:58 +0530

    entrypoint bug fix

--- mongo/entrypoint.sh
+++ mongo/entrypoint.sh
@@ -10,6 +10,8 @@ cp $KOBO_DOCKER_SCRIPTS_DIR/init_* /docker-entrypoint-initdb.d/
 
 bash $KOBO_DOCKER_SCRIPTS_DIR/upsert_users.sh
 
+#ls -la
+ln usr/local/bin/docker-entrypoint.sh entrypoint.sh
 echo "Launching official entrypoint..."
 # `exec` here is important to pass signals to the database server process;
 # without `exec`, the server will be terminated abruptly with SIGKILL (see #276)
